83eb72c5e823b2a21b394379cfc421e167c5d077
Jean-Baptiste Rieu <jb.rieu@gmail.com>
2015-09-04 13:35:59 +0200

    Semantic correction on field value in Measures (only one parent sensor, not many)

--- models/Measures.js
+++ models/Measures.js
@@ -4,7 +4,7 @@ var MeasureSchema = new mongoose.Schema({
     value: {type: Number, required: true},
     date: {type: Date, required: true},
     comment: String,
-    sensors: {type: mongoose.Schema.Types.ObjectId, ref: 'Sensor'}
+    sensor: {type: mongoose.Schema.Types.ObjectId, ref: 'Sensor'}
 });
 
 mongoose.model('Measure', MeasureSchema);
