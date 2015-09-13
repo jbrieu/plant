88f141abe71b2d5e5a6c2f2637d436e5165bdd1f
Jean-Baptiste Rieu <jb.rieu@gmail.com>
2015-09-13 22:00:54 +0200

    Explain how to use JWT with API Calls

--- README.md
+++ README.md
@@ -7,10 +7,19 @@ This app is independent from the hardware setup, meaning you can collect from di
 To install Node modules run : 
 `npm install` 
 
-To run do :
+To run do (mongod must be started) :
 `npm start`
 
 It's configured to run on locahost:4000 (and not the default 3000) for personal convenience :D
 
 The User methods needs an environement variable as a secret key (for Json Web Token generation)
 Create a variable named PLANT_SECRET_KEY in you env.
+
+To use the API with a secured user (POST and PUT actions) : 
+ * register a new user for the API access in the interface
+ * go to your localStorage with your browser developper tool
+ * copy the value for the field : "plant-token" => it's the [JWT](jwt.io) for this user
+ * in your API call add a header field "Authorization"
+ * populate the value with "Bearer " followed by your JWT string
+ 
+ 
--- models/Sensors.js
+++ models/Sensors.js
@@ -1,13 +1,31 @@
 var mongoose = require('mongoose');
 
 var SensorSchema = new mongoose.Schema({
-    sensorName: {type: String, required: true},
-    valueType: {type: String, required: true},    
-    minValue: {type: Number, default: 0},
-    maxValue: {type: Number, default: 1024},    
+    sensorName: {
+        type: String,
+        required: true
+    },
+    valueType: {
+        type: String,
+        required: true
+    },
+    minValue: {
+        type: Number,
+        default: 0
+    },
+    maxValue: {
+        type: Number,
+        default: 1024
+    },
     description: String,
-    measures : [{type: mongoose.Schema.Types.ObjectId, ref: 'Measure' }],
-    plants :  [{type: mongoose.Schema.Types.ObjectId, ref: 'Plant' }],
+    measures: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Measure'
+    }],
+    plants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Plant'
+    }],
     addedBy: String
 });
 
